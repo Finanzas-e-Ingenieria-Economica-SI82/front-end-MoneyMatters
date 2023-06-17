fe37960a96abffd4a9312225bce7cc5de68c2403
DiegoDeLaFlor <u202017237@upc.edu.pe>
2023-06-17 16:36:18 -0500

    feat: :fire: Added download excel Button

--- src/views/FrenchMethod.vue
+++ src/views/FrenchMethod.vue
@@ -7,7 +7,7 @@
 		<!-- <div class="p-field">
 			<label for="importe">Valor de la Propiedad :  s/.{{ property.price }}  </label>
 		</div> -->
-
+		<p style="font-weight: bold;">... del préstamo</p>
 		<div class="p-field">
 			<label for="importe">Valor de la propiedad (S/.):</label>
             <input class="form-control" v-model="importe" :min="0" :max="100">
@@ -42,6 +42,7 @@
 			<label style="font-weight: bold;">(meses)</label>
 		</div>
 
+		<p style="font-weight: bold;">... de los costes/gastos iniciales</p>
 		<div class="p-field">
 			<label for="coste_notarial">Coste notarial:</label>
             <input class="form-control" v-model="coste_notarial" :min="1" :max="1000">
@@ -56,7 +57,8 @@
 			<label for="comision">Comisión de estudio:</label>
             <input class="form-control" v-model="comision" :min="1" :max="1000">
 		</div>
-
+		
+		<p style="font-weight: bold;">... de los costes/gastos periodicos</p>
 		<div class="p-field">
 			<label for="portes">Portes:</label>
             <input class="form-control" v-model="portes" :min="1" :max="1000">
@@ -79,6 +81,7 @@
 			<label style="font-weight: bold;">(Anual)</label>
 		</div>
 
+		<p style="font-weight: bold;">... del costo de oportunidad</p>
 		<div class="p-field">
 			<label for="COK"> % dscto. COK:</label>
             <input class="form-control" v-model="COK" :min="1" :max="50" suffix="%">
@@ -88,6 +91,7 @@
 		
 		<div>
 			<pv-button style="background: #46A2AE; border-style: none; width: 10%; justify-content: center; font-weight: bold;" label="Calcular" @click="calcular"></pv-button>
+			<pv-button style="background: #46A2AE; border-style: none; width: 10%; justify-content: center; font-weight: bold; margin-left: 15px; " label="Descargar" @click="descargarExcel"></pv-button>
 		</div>
 		<br>
 		<div id="resultado">
@@ -111,7 +115,7 @@
 				<!-- <div>Cuota a pagar mensualmente: {{ cuotaMensual.toLocaleString('es-US', {minimumFractionDigits: 2, maximumFractionDigits: 2}) }} $</div>
 				<div>Capital Inicial: {{ importe.toLocaleString('es-US', {minimumFractionDigits: 2, maximumFractionDigits: 2}) }} $</div> -->
 			</div>
-<br> 
+			<br> 
 			<pv-datatable v-if="tabla.length > 0" :value="tabla" :rows="tabla.length">
 				<pv-column field="periodo" header="Periodo"></pv-column>
 				<pv-column field="saldo" header="Saldo"></pv-column>
@@ -133,8 +137,9 @@
 </template>
 
 <script>
-import AppBar from '../components/AppBar.vue';
-	import ApiService from '@/services/ApiService';
+	import AppBar from '../components/AppBar.vue';
+	import { utils, writeFile } from 'xlsx';
+	//import ApiService from '@/services/ApiService';
     export default{
     name: 'FrenchMethod',
     components: { AppBar },
@@ -232,22 +237,13 @@ import AppBar from '../components/AppBar.vue';
 				}
 
 				this.totalIntereses = totalInt;
-			}
-
-			
-
+			},
+			descargarExcel() {
+				const workbook = utils.table_to_book(document.getElementById('resultado'));
+				const fileName = 'Método Francés.xlsx';
+				writeFile(workbook, fileName);
+			},
 		},
-
-		created() {
-		ApiService.getPropertiesById(this.property).then((response) => {
-        this.property = response.data;
-				console.log("property:", this.property);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	}
-	
     }
 	
 </script>
